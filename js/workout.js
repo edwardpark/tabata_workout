e42e58516759f2f0cb8feec126ff34adab902448
Edward Park <parkes.edward@gmail.com>
2015-09-17 12:02:03 -0400

    added inital start script

--- js/workout.js
+++ js/workout.js
@@ -1,7 +1,9 @@
-(function(){
+angular.module('tabataApp').controller('WorkoutController',['$scope', function($scope){
+    var restPeriod;
+    var workoutPlan;
+    var startProgram = functon(){
+      startWorkout();
+    }
+    startProgram(); //use traditional because want to control when start
 
-
-  angular.module('tabataApp').controller('WorkoutController',function($scope){});
-
-
-})();
+}]);
