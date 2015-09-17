cdccb33831da0817dd5a67c7392466473d529d74
Edward Park <parkes.edward@gmail.com>
2015-09-17 12:04:13 -0400

    added exercise and workout plan instances

--- js/workout.js
+++ js/workout.js
@@ -1,4 +1,19 @@
 angular.module('tabataApp').controller('WorkoutController',['$scope', function($scope){
+  function Exercise(args) {
+      this.name = args.name;
+      this.title = args.title;
+      this.description = args.description;
+      this.image = args.image; //reference that marvel tutorial about this pattern
+      this.related = {};
+  }
+  
+  function WorkoutPlan(args) {
+      this.exercises = [];
+      this.name = args.name;
+      this.title = args.title;
+      this.restBetweenExercise = args.restBetweenExercise;
+  };
+
     var restPeriod;
     var workoutPlan;
     var startProgram = functon(){
