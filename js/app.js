e15fa106390426a3ce8ce67caf9b9b6448e8ce76
Edward Park <parkes.edward@gmail.com>
2015-09-18 11:11:51 -0400

    added routes , files and cdn

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 
-  angular.module('app', ['tabataWorkout']);
+  angular.module('app', ['ngRoute','tabataWorkout'])
+  .config(function ($routeProvider) {
+        $routeProvider.when('/start', {
+            templateUrl: 'views/start.html'
+        });
+        $routeProvider.when('/workout', {
+            templateUrl: 'views/workout.html',
+            controller: 'WorkoutController'
+        });
+
+    });
 
   angular.module('tabataWorkout', []);
